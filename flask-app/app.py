7a0047549d55e4ae9f4c1baa94fff3d66ea2a448
Harish Vadaparty <harishvadaparty@gmail.com>
2023-12-16 16:31:01 +0530

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -102,5 +102,5 @@ def get_speakers():
 
 # Starting the Flask app
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5002)
+    app.run(debug=True, host="0.0.0.0", port=5002) # 5000 is confliting for  MAC's AirDrop Utils, so making it 5002
 
